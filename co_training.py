161e53ea2ab5996ec6589e5aacc8cdc6a960c89b
RcyuH <phunghuy.ms@gmail.com>
2025-02-23 15:16:10 +0700

    initialize files

---
+++ co_training.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 15:12:41 2025
+
+@author: rcyuh
+"""
+
---
+++ item_embedding.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 23 15:13:08 2025
+
+@author: rcyuh
+"""
+
