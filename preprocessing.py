65ce6c3c4cd33822cf95fd4e20c07a646502c927
RcyuH <phunghuy.ms@gmail.com>
2025-02-23 17:21:07 +0700

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -22,5 +22,58 @@ preprocessing for items meta data
     topic: topic1 > topic2 > topic3
     type (cần một cái tên chuẩn hơn): Mô tả về type
     problem_type (cần một cái tên chuẩn hơn): Mô tả về problem_type và số câu có problem_type đó 
-    difficulty: Correct_avg đã chuẩn hóa
+    difficulty (theo thang điểm 0-1): Correct_avg đã chuẩn hóa
 """
+
+import pandas as pd
+from collections import Counter
+
+# Constant
+cols_needed = ["problem_id", "sequence_id", "skill", "problem_type", "type", "correct"]
+file_path = "/home/rcyuh/Desktop/1. Đồ án tốt nghiệp/Co-supervised/assistment_2012_2013.csv"
+nrows=1000
+
+class preprocessing_meta_data:
+    def __init__(self, file_path, nrows, cols_needed):
+        self.df = pd.read_csv(file_path, usecols=cols_needed, nrows=nrows)
+        
+        # Group by problem_id
+        self.df = self.df.groupby("problem_id").agg({
+            "sequence_id": "first",  # Giữ giá trị đầu tiên
+            "skill": "first",
+            "problem_type": "first",
+            "type": "first",
+            "correct": "mean"  # Tính trung bình
+        }).reset_index()
+        
+        self.df.rename(columns={"correct": "correct_avg"}, inplace=True)
+        
+        self.df["skill"] = self.df["skill"].fillna("Unknown")
+        
+    def group_by_sequence(self):
+        self.df = self.df.groupby("sequence_id").agg({
+            "skill": list,
+            "problem_type": list,
+            "type": "first",
+            "correct_avg": "mean",
+            "problem_id": "count"
+        }).reset_index()
+        
+        self.df.rename(columns={"problem_id": "amount"}, inplace=True)
+        self.df.rename(columns={"correct_avg": "difficulty"}, inplace=True)
+        self.df.rename(columns={"skill": "topic"}, inplace=True)
+        self.df.rename(columns={"type": "sequence_type"}, inplace=True)
+        
+        self.df["topic"] = self.df["topic"].apply(lambda x: dict(Counter(x)))
+        self.df["problem_type"] = self.df["problem_type"].apply(lambda x: dict(Counter(x)))
+        
+        self.df["topic"] = self.df["topic"].apply(lambda x: sorted(x, key=x.get, reverse=True) if isinstance(x, dict) else x)
+
+    def process(self):
+        self.group_by_sequence()
+        
+        return self.df
+
+        
+pre = preprocessing_meta_data(file_path, nrows, cols_needed)
+df = pre.process()
